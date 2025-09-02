fe324bf36badcae27ea7b561427ef42c7ae06902
Thomas Hartmann <thomas@webtrade.dk>
2025-09-02 10:16:38 +0200

    update to pom

--- src/main/java/dk/ek/DebuggingDemo.java
+++ src/main/java/dk/ek/DebuggingDemo.java
@@ -3,25 +3,34 @@ package dk.ek;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 public class DebuggingDemo {
     public static void main(String[] args) {
         demoCollection();
+//        String name = null;
+//
+//        Integer namelength = Integer.parseInt(name);
+////        System.out.println(name.length());
     }
 
     public static void demoCollection() {
         DebuggingDemo demo = new DebuggingDemo();
 
-//        System.out.println("Out of Bound");
-//        demo.outOfBoundDemo();
+//            System.out.println("Out of Bound");
+//            demo.outOfBoundDemo();
 
-//        System.out.println("NullPointer");
-//        demo.nullPointerDemo();
+        System.out.println("NullPointer");
+        demo.nullPointerDemo();
 
 //        System.out.println("Infinite loop");
 //        demo.infiniteLoop();
 
-        System.out.println("Calculate error");
-        demo.calculateError();
+//        System.out.println("Calculate error");
+//        demo.calculateError();
+
+//        System.out.println("Stream debugging");
+//        demo.streamDebugging()iwull;
     }
 
     public void outOfBoundDemo() {
@@ -54,6 +63,24 @@ public class DebuggingDemo {
     public void calculateError(){
         int result = new Calculator().multiply(4, 5);
         System.out.println("Result: "+ result);
+    }
+
+    /**
+     * Use 'Trace current stream chain' in IntelliJ to debug the stream
+     */
+    public void streamDebugging(){
+        List<Student> students = List.of(
+                new Student("Alice", 85),
+                new Student("Bob", 70),
+                new Student("Charlie", 90),
+                new Student("David", 60)
+        );
+        double average = students.stream()
+                .filter(s -> s.grade >= 75)
+                .mapToInt(s -> s.grade)
+                .average()
+                .orElse(0);
+        System.out.println("Average grade of students with grade >= 75: " + average);
     }
 
     @Data
---
+++ src/test/java/dk/ek/exercises/Ex01BankAccountTest.java
@@ -0,0 +1,10 @@
+package dk.ek.exercises;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex01BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void ex1() {
+    }
+}
